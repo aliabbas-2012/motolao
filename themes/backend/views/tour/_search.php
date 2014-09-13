9ba6cd20c6400b119ac4020d0b83797833ffab7f
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-13 23:41:17 +0500

    git ignore with search

--- themes/backend/views/tour/_search.php
+++ themes/backend/views/tour/_search.php
@@ -13,15 +13,37 @@
                 <div class="row">
                     <div class="col-lg-9">
 
-                        <?php $form=$this->beginWidget('CActiveForm', array(
-	'action'=>Yii::app()->createUrl($this->route),
-	'method'=>'get',
-)); ?>
+                        <?php
+                        $form = $this->beginWidget('CActiveForm', array(
+                            'action' => Yii::app()->createUrl($this->route),
+                            'method' => 'get',
+                        ));
+                        ?>
 
-                                                                                                                                                                                                                                                                                                                                                                                                                                                        <div class="form-group">
+                        <div class="form-group">
+                            <?php echo $form->label($model, 'name', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textField($model, 'name', array('class' => 'form-control', 'maxlength' => 150)); ?>
+                                <?php echo $form->error($model, 'name'); ?>
+
+                            </div>
+
+                        </div><!-- group -->
+
+
+                        <div class="form-group">
+                            <?php echo $form->label($model, 'short_title', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textField($model, 'short_title', array('class' => 'form-control', 'maxlength' => 150)); ?>
+                                <?php echo $form->error($model, 'short_title'); ?>
+
+                            </div>
+
+                        </div><!-- group -->
+                        <div class="form-group">
                             <div class="col-lg-2"></div>
                             <div class="col-lg-6 input-group-btn">
-                                <?php echo CHtml::submitButton('Search',array('class' => 'btn btn-primary')); ?>
+                                <?php echo CHtml::submitButton('Search', array('class' => 'btn btn-primary')); ?>
                             </div>
                         </div>
 
