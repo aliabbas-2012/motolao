1d6f8ba1ed3972aa94fcc6995ea610bd29de7a9a
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-13 20:54:39 +0500

    Code again cruds

--- themes/backend/views/layouts/column2.php
+++ themes/backend/views/layouts/column2.php
@@ -31,93 +31,14 @@
                 </div>
                 <!--end search section-->
             </li>
-            <li class="selected">
+            <li class="<?php echo $this->id == "site" && $this->action->id == "index" ? "selected" : "" ?>">
                 <a href="<?php echo $this->createUrl("/site/index"); ?>"><i class="fa fa-dashboard fa-fw"></i>Dashboard</a>
             </li>
-            <li>
-                <a href="javascript:void(0)"><i class="fa fa-bar-chart-o fa-fw"></i> Charts<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "flot")); ?>">Flot Charts</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "morris")); ?>">Morris Charts</a>
-                    </li>
-                </ul>
-                <!-- second-level-items -->
-            </li>
-            <li>
-                <a href="<?php echo $this->createUrl("/site/page/", array("view" => "timeline")); ?>"><i class="fa fa-flask fa-fw"></i>Timeline</a>
-            </li>
-            <li>
-                <a href="<?php echo $this->createUrl("/site/page/", array("view" => "tables")); ?>"><i class="fa fa-table fa-fw"></i>Tables</a>
-            </li>
-            <li>
-                <a href="<?php echo $this->createUrl("/site/page/", array("view" => "forms")); ?>"><i class="fa fa-edit fa-fw"></i>Forms</a>
-            </li>
-            <li>
-                <a href="javascript:void(0)"><i class="fa fa-wrench fa-fw"></i>UI Elements<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "panels-wells")); ?>">Panels and Wells</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "buttons")); ?>">Buttons</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "notifications")); ?>">Notifications</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "typography")); ?>">Typography</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "grid")); ?>">Grid</a>
-                    </li>
-                </ul>
-                <!-- second-level-items -->
-            </li>
-            <li>
-                <a href="javascript:void(0)"><i class="fa fa-sitemap fa-fw"></i>Multi-Level Dropdown<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="javascript:void(0)">Second Level Item</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0)">Second Level Item</a>
-                    </li>
-                    <li>
-                        <a href="javascript:void(0)">Third Level <span class="fa arrow"></span></a>
-                        <ul class="nav nav-third-level">
-                            <li>
-                                <a href="javascript:void(0)">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">Third Level Item</a>
-                            </li>
-                            <li>
-                                <a href="javascript:void(0)">Third Level Item</a>
-                            </li>
-                        </ul>
-                        <!-- third-level-items -->
-                    </li>
-                </ul>
-                <!-- second-level-items -->
-            </li>
-            <li>
-                <a href="javascript:void(0)"><i class="fa fa-files-o fa-fw"></i>Sample Pages<span class="fa arrow"></span></a>
-                <ul class="nav nav-second-level">
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/page/", array("view" => "blank")); ?>">Blank Page</a>
-                    </li>
-                    <li>
-                        <a href="<?php echo $this->createUrl("/site/login"); ?>">Login Page</a>
-                    </li>
-                </ul>
-                <!-- second-level-items -->
-            </li>
+            <?php
+            if (strstr(Yii::app()->request->hostInfo, "localhost")) {
+                $this->renderPartial("//layouts/_menus_local_host");
+            }
+            ?>
         </ul>
         <!-- end side-menu -->
     </div>
@@ -125,7 +46,7 @@
 <!-- end sidebar-collapse -->
 <!--  page-wrapper -->
 <div id="page-wrapper">
-    <?php echo $content; ?>
+<?php echo $content; ?>
 </div>
 <!-- end page-wrapper -->
 <?php $this->endContent(); ?>
