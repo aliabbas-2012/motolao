a7271d0b50fd7666a4e3a0856fab070c47a0c8e4
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-14 20:34:15 +0500

    git add all migration of tour images

--- themes/backend/views/tour/view.php
+++ themes/backend/views/tour/view.php
@@ -66,15 +66,15 @@ Yii::app()->getClientScript()->registerScriptFile(Yii::app()->theme->baseUrl . '
     <div class="col-lg-12">
         <div class="panel-body">
             <ul class="nav nav-tabs">
-                <li class="active"><a href="#home" data-toggle="tab">Tour Languages</a>
+                <li class="active"><a href="#languages" data-toggle="tab">Tour Languages</a>
                 </li>
-                <li><a href="#profile" data-toggle="tab">Profile</a>
+                <li><a href="#images" data-toggle="tab">Tour Images</a>
                 </li>
 
             </ul>
 
             <div class="tab-content">
-                <div class="tab-pane fade in active" id="home">
+                <div class="tab-pane fade in active" id="languages">
                     <?php
                     $criteria = new CDbCriteria();
                     $criteria->addCondition("parent_id =" . $model->id);
@@ -88,7 +88,7 @@ Yii::app()->getClientScript()->registerScriptFile(Yii::app()->theme->baseUrl . '
                     ?>
                     <?php $this->renderPartial("//tour/_lang_form", array("model" => $model->tour_langs, "id" => $model->id)); ?>
                 </div>
-                <div class="tab-pane fade" id="profile">
+                <div class="tab-pane fade" id="images">
                     <h4>Profile Tab</h4>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
                 </div>
