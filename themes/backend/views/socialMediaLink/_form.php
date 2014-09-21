72fdc1efdbe23aa830dab9004da72133cfb95040
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-21 20:15:25 +0500

    added tinymc extension in forms all

--- themes/backend/views/socialMediaLink/_form.php
+++ themes/backend/views/socialMediaLink/_form.php
@@ -9,13 +9,14 @@
                 <div class="row">
                     <div class="col-lg-9">
 
-                        
-                        <?php                         $form = $this->beginWidget('CActiveForm', array(
-                        'id' => 'social-media-link-form',
-                        'enableAjaxValidation' => false,
-                        'htmlOptions' => array(
-                        'class' => 'form-horizontal'
-                        )
+
+                        <?php
+                        $form = $this->beginWidget('CActiveForm', array(
+                            'id' => 'social-media-link-form',
+                            'enableAjaxValidation' => false,
+                            'htmlOptions' => array(
+                                'class' => 'form-horizontal'
+                            )
                         ));
                         ?>
 
@@ -23,86 +24,90 @@
                             <?php echo Yii::t('app', 'Fields with'); ?> <span class="required">*</span> <?php echo Yii::t('app', 'are required'); ?>.
                         </p>
 
-                        <?php echo $form->errorSummary($model,'', '',array('class' => 'alert alert-block alert-danger')); ?>
+                        <?php echo $form->errorSummary($model, '', '', array('class' => 'alert alert-block alert-danger')); ?>
 
-                                                                                                          
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'lang_id',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textField($model, 'lang_id', array('class' => 'form-control','maxlength' => 11)); ?>
-                                        <?php echo $form->error($model,'lang_id'); ?>
- 
-                                    </div>
 
-                                </div><!-- group -->
-                                                                                  
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'lang_id', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php
+                                $criteria = new CDbCriteria();
+                                $languages = array("" => "Select") + CHtml::listData(Language::model()->findAll($criteria), "id", "name");
+                                echo $form->dropDownList($model, 'lang_id', $languages, array('class' => 'form-control'));
+                                ?>
+                                <?php echo $form->error($model, 'lang_id'); ?>
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'title',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textField($model, 'title', array('class' => 'form-control','maxlength' => 150)); ?>
-                                        <?php echo $form->error($model,'title'); ?>
- 
-                                    </div>
+                            </div>
 
-                                </div><!-- group -->
-                                                                                  
+                        </div><!-- group -->
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'url',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textField($model, 'url', array('class' => 'form-control','maxlength' => 150)); ?>
-                                        <?php echo $form->error($model,'url'); ?>
- 
-                                    </div>
 
-                                </div><!-- group -->
-                                                                                  
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'title', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textField($model, 'title', array('class' => 'form-control', 'maxlength' => 150)); ?>
+                                <?php echo $form->error($model, 'title'); ?>
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'meta_tag',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textArea($model, 'meta_tag',array('class' => 'form-control')); ?>
-                                        <?php echo $form->error($model,'meta_tag'); ?>
- 
-                                    </div>
+                            </div>
 
-                                </div><!-- group -->
-                                                                                  
+                        </div><!-- group -->
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'meta_description',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textArea($model, 'meta_description',array('class' => 'form-control')); ?>
-                                        <?php echo $form->error($model,'meta_description'); ?>
- 
-                                    </div>
 
-                                </div><!-- group -->
-                                                                                                                                                                                                                                                                                                  
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'url', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textField($model, 'url', array('class' => 'form-control', 'maxlength' => 150)); ?>
+                                <?php echo $form->error($model, 'url'); ?>
 
-                                <div class="form-group">
-                                    <?php echo $form->labelEx($model,'activity_log',array('class' => 'control-label col-lg-2')); ?>
-                                    <div class="col-lg-4">
-                                        <?php echo $form->textArea($model, 'activity_log',array('class' => 'form-control')); ?>
-                                        <?php echo $form->error($model,'activity_log'); ?>
- 
-                                    </div>
+                            </div>
 
-                                </div><!-- group -->
-                                                    
+                        </div><!-- group -->
+
+
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'meta_tag', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textArea($model, 'meta_tag', array('class' => 'form-control')); ?>
+                                <?php echo $form->error($model, 'meta_tag'); ?>
+
+                            </div>
+
+                        </div><!-- group -->
+
+
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'meta_description', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textArea($model, 'meta_description', array('class' => 'form-control')); ?>
+                                <?php echo $form->error($model, 'meta_description'); ?>
+
+                            </div>
+
+                        </div><!-- group -->
+
+
+                        <div class="form-group">
+                            <?php echo $form->labelEx($model, 'activity_log', array('class' => 'control-label col-lg-2')); ?>
+                            <div class="col-lg-4">
+                                <?php echo $form->textArea($model, 'activity_log', array('class' => 'form-control')); ?>
+                                <?php echo $form->error($model, 'activity_log'); ?>
+
+                            </div>
+
+                        </div><!-- group -->
+
                         <div class="form-group">
                             <div class="col-lg-2"></div>
                             <div class="col-lg-6 input-group-btn">
 
-                                
 
-    <?php echo CHtml::submitButton('Save', array('class' => 'btn btn-primary')); ?>
-     
-<?php
-$this->endWidget();
-?>
+
+                                <?php echo CHtml::submitButton('Save', array('class' => 'btn btn-primary')); ?>
+
+                                <?php
+                                $this->endWidget();
+                                ?>
                             </div>
                         </div>
 
