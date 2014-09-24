0870c3500245a9d38d89e4f626f5f0d81bbc55df
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-25 00:04:17 +0500

    Banners

--- themes/frontend/views/pluggins/banner.php
+++ themes/frontend/views/pluggins/banner.php
@@ -2,29 +2,37 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel"> 
 
     <div class="carousel-inner">
-        <div class="item active"> 
-            <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/slider-index/slider.jpg" style="width:100%" alt="First slide" />
-            <div class="container">
-                <div class="carousel-caption">
-                  <!--<p><a class="btn btn-lg btn-primary" href="javascript:void(0)" role="button">Sign up today</a></p>-->
+        <?php
+        $criteria = new CDbCriteria();
+        $criteria->addCondition("t.key=:key AND lang_id = :lang_id");
+        $criteria->params = array(
+            ":key" => $this->page_key,
+            ":lang_id" => $this->lang_id,
+        );
+        $sliders = Banner::model()->findAll($criteria);
+        $count = 0;
+        
+        foreach ($sliders as $slider):
+            ?>
+            <div class="item <?php echo $count ==0?"active":""; ?>" >
+                
+                <?php
+                  echo CHtml::image($slider->image_url['image_large'],$slider->alt,array(
+                      "title"=>$slider->title,
+                      "style"=>"width:100%",
+                   ));
+                ?>
+                <div class="container">
+                    <div class="carousel-caption">
+                      <!--<p><a class="btn btn-lg btn-primary" href="javascript:void(0)" role="button">Sign up today</a></p>-->
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="item">
-            <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/slider-index/slider.jpg" style="width:100%" data-src="" alt="Second    slide" />
-            <div class="container">
-                <div class="carousel-caption">
-                  <!--<p><a class="btn btn-lg btn-primary" href="javascript:void(0)" role="button">Learn more</a></p></p>-->
-                </div>
-            </div>
-        </div>
-        <div class="item"> 
-            <img src="<?php echo Yii::app()->theme->baseUrl; ?>/assets/img/slider-index/slider.jpg" style="width:100%" data-src="" alt="Third slide" />
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+            <?php
+            $count++;
+        endforeach;
+        ;
+        ?>
     </div>
     <a class="left carousel-control" href="#myCarousel" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a> <a class="right carousel-control" href="#myCarousel" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a> 
 </div>
