3c446d9d836ad0a8e1e073d264b6e858d850590a
Ali Abbas <itsgeniusstar@gmail.com>
2014-09-28 22:55:16 +0500

    Site map heading

--- themes/frontend/views/default/sitemap.php
+++ themes/frontend/views/default/sitemap.php
@@ -1,8 +1,20 @@
-<?php
+<!--start: Container -->
+<div class="container">
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+    <!-- start: Hero Unit - Main hero unit for a primary marketing message or call to action -->
+    <div id="signwrapper">
+        <div class="logosign">
+            <?php echo CHtml::image(Yii::app()->theme->baseUrl . "/assets/img/logo.png", Yii::t("sitemap", "sitemap Logo"), array("title" => Yii::t("sitemap", "sitemap Logo"))); ?>
+        </div>
+    </div>
+    <h2><?php echo Yii::t("sitemap", "sitemap Heading"); ?></h2>
+    <div class="hero-unit">
 
+        <p>
+            <?php echo Yii::t("sitemap", "sitemap Summary"); ?>
+        </p>
+
+    </div>
+    <!-- end: Hero Unit -->
+</div>
+<!--end: Container-->
