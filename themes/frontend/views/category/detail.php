ddd77d5513a0f32523dbe8e9fe8389376d7b7986
Ali Abbas <itsgeniusstar@gmail.com>
2014-11-30 17:51:35 +0500

    5) On the category Quick Search links will contradict the category

--- themes/frontend/views/category/detail.php
+++ themes/frontend/views/category/detail.php
@@ -1,4 +1,6 @@
-<!--start: Container -->
+
+
++<!--start: Container -->
 <div class="container">
     <!-- start: Hero Unit - Main hero unit for a primary marketing message or call to action -->
     <div id="signwrapper">
@@ -17,23 +19,30 @@
     <div class="menu span12">
 
         <a href="<?php echo $this->createUrl("/web/category/index", array('category' => $model->url)) ?>" title="" class="viewtours"><?php echo Yii::t("category", "VIEW " . $model->name . " TOURS"); ?></a>
-        <li class="dropdown">
-            <a href="#" class="dropdown-toggle-tour" data-toggle="dropdown">
-                <?php echo Yii::t("category", "Quick Search"); ?>
-                <b class="caret"></b></a>
-            <ul class="dropdown-menu">
-                <?php
-                $criteria = new CDbCriteria();
-                $criteria->limit = '12';
-                $tours = Tour::model()->findAll();
-                foreach ($tours as $tour) {
-                    ?>
-                    <li><a href="<?php echo $this->createUrl("/web/category/detail", array("category" => $tour->category->url, "slug" => $tour->url)); ?>" title=""><?php echo $tour->name ?> I <?php echo $tour->category->name; ?></a></li>
+        <?php
+        $criteria = new CDbCriteria();
+        $criteria->limit = '12';
+        $criteria->addCondition("category_id <> " . $model->category->id);
+        if ($tours = Tour::model()->findAll($criteria)) {
+            ?>
+            <li class="dropdown">
+                <a href="#" class="dropdown-toggle-tour" data-toggle="dropdown">
+                    <?php echo Yii::t("category", "Quick Search"); ?>
+                    <b class="caret"></b></a>
+                <ul class="dropdown-menu">
                     <?php
-                }
-                ?>
-            </ul>
-        </li>
+                    foreach ($tours as $tour) {
+                        ?>
+                        <li><a href="<?php echo $this->createUrl("/web/category/detail", array("category" => $tour->category->url, "slug" => $tour->url)); ?>" title=""><?php echo $tour->name ?> I <?php echo $tour->category->name; ?></a></li>
+                        <?php
+                    }
+                    die;
+                    ?>
+                </ul>
+            </li>
+            <?php
+        }
+        ?>
 
     </div>
 </div>
@@ -63,10 +72,10 @@
     <div class="span7">
 
         <h2><?php echo $model->name; ?></h2>
-        <h4><?php echo $model->short_title;
-            ;
+        <h4><?php
+            echo $model->short_title;
             ?></h4>
-<?php echo $model->description; ?>
+            <?php echo $model->description; ?>
     </div>
 
 </div>
