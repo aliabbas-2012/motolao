fcf2773bb834e5d1a86a13b70b59d2def1f88c05
Ali Abbas <itsgeniusstar@gmail.com>
2014-12-02 00:12:21 +0500

    10) default image of popup for video will show its a video

--- themes/frontend/views/default/_gallery_partial.php
+++ themes/frontend/views/default/_gallery_partial.php
@@ -4,6 +4,7 @@ foreach ($gallaries as $gallery):
     <li class="span4">
         <?php
         $url = $gallery->image_url['image_large'];
+        $data_poster = "";
         $content_type = pathinfo($url);
         if (!isset($info['extension'])) {
             $content_type['extension'] = "jpg";
@@ -16,9 +17,12 @@ foreach ($gallaries as $gallery):
 
             $url = $gallery->video_tag_embedded_code;
             $content_type = $gallery->_content_type;
+            $data_poster = "data-poster='".$gallery->image_url['image_large']."'";
         endif;
         ?>
-        <a fullscreen="false" type="<?php echo $content_type; ?>" data-gallery="" href="<?php echo $url; ?>" 
+        <a <?php echo $data_poster ?> 
+            fullscreen="false" type="<?php echo $content_type; ?>" data-gallery="" href="<?php echo $url; ?>" 
+           
            class="thumbnail <?php echo $gallery->land_scape; ?>" target="_blank">
             <?php
             echo CHtml::image($gallery->image_url['image_large'], $gallery->alt, array(
